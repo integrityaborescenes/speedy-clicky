5c3e2196b6041bc729b9781c7d6ca08a609d03d2
Vladislav Shitov <sayroxskrim@bk.ru>
2025-10-24 14:50:29 +0400

    add keyboard input block

---
+++ src/components/keyboard__preview/index.js
@@ -0,0 +1,6 @@
+export default () =>
+    `
+    <div class="keyboard__preview">
+       
+    </div>
+    `
--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
-import './styles/main.scss'
+import './styles/main.scss';
+import keyboard__input from "./components/keyboard__input";
+import keyboard__preview from "./components/keyboard__preview";
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-//
-// setupCounter(document.querySelector('#counter'))
+document.querySelector('.typing-test-container').innerHTML = `
+  ${keyboard__input()}
+  ${keyboard__preview()}
+`
+
